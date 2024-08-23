cc6685d6e89617a10ab97bcb337d97962f4cfcaf
Aditya24453 <adi240705kr@gmail.com>
2024-08-23 08:47:07 +0530

    hi sir

---
+++ file2.js
@@ -0,0 +1,10 @@
+//nn ss bb u
+let a = null;
+let b = 509;
+let c = false;
+let d = Symbol("aditya")
+let e = BigInt(585) + BigInt("9")
+let f = "hello world"
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d )
---
+++ javascript1.js
@@ -0,0 +1,5 @@
+console.log("hello worlthis progtram adds three number")
+let a = 78
+console.log(a)
+a ="Aditya"
+console.log(a)
